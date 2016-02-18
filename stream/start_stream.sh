30ce54c9c444611a6f9afad8a2dbbdf1dd36003d
Shubhojyoti Ganguly <gangulyshubhojyoti@gmail.com>
2016-02-19 02:44:00 +0530

    Update start_stream.sh

--- stream/start_stream.sh
+++ stream/start_stream.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if [ "$(uname -m)"=="armv6l" ]
-    sudo modprobe bcm2835-v4l2
+if [ "$(uname -m)"=="armv6l" ];
+then
+    sudo modprobe bcm2835-v4l2;
 fi
 
 sudo ffmpeg -y -f v4l2 -i /dev/video0 -update 1 -r 30 /var/www/html/vrstream/tmp/output.jpg
